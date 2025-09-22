8ee6415205ae5f94a15e2cce0c936fd1c905c4d3
Lizeht Gutierrez <lauralizethgutierrez8@gmail.com>
2025-09-22 08:13:49 -0500

    ..

--- src/brands/car.service.ts
+++ src/brands/car.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,14 +6,30 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class CarService {
     constructor(private prisma:PrismaService){}
     async create(newCar: CreateCarDto){
-        return await this.prisma.car.create({
+        //validacion logica de negocio
+        //la placa no se repita
+        let existe=await this.prisma.car.findFirst({
+            where:{plate:newCar.plate}
+        })
+        if(existe) throw new HttpException("la placa ya existe",400)
+        else{return await this.prisma.car.create({
             data:{
                 plate:newCar.plate,
                 release_date: new Date(newCar.release_date),
                 is_available: newCar.is_available ,
                 price: newCar.price,
-                brand_id: newCar.brand_id, 
+                brand:{connect:{id:newCar.brand_id}
+                }
             }
+        })}
+    }
+    async findByid(id:number){
+        //1.buscar el carro por id
+        let existe =await this.prisma.car.findFirst({
+            where:{id:id}
         })
+        //2.si no existe lanzar una http exception
+        //3.si existe devolver el carro
+        
     }
 }
